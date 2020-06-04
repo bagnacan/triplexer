111747f5a91d7570854879290f8c1aedba5f78f4
Andrea Bagnacani <andrea.bagnacani@uni-rostock.de>
2020-06-04 15:13:50 +0200

    moved CLI options to relative module

--- cli.py
+++ cli.py
@@ -8,6 +8,65 @@ from common import *
 
 
 
+# system setting arguments
+#
+OPT_NAMESPACE       = "ns"
+OPT_NAMESPACE_SHORT = str("-" + OPT_NAMESPACE[:1])
+OPT_NAMESPACE_EXT   = str("--" + OPT_NAMESPACE)
+OPT_CORES       = "exe"
+OPT_CORES_SHORT = str("-" + OPT_CORES[1:2])
+OPT_CORES_EXT   = str("--" + OPT_CORES[:3])
+OPT_DB       = "db"
+OPT_DB_SHORT = str("-" + OPT_DB[:1])
+OPT_DB_EXT   = str("--" + OPT_DB)
+
+
+# operation arguments
+#
+
+# read
+OPT_READ         = "read"
+OPT_READ_SHORT   = str("-" + OPT_READ[:1])
+OPT_READ_EXT     = str("--" + OPT_READ)
+
+# filtrate
+OPT_FILTRATE         = "filtrate"
+OPT_FILTRATE_SHORT   = str("-" + OPT_FILTRATE[:1])
+OPT_FILTRATE_EXT     = str("--" + OPT_FILTRATE)
+
+# init namespaces
+OPT_INIT_NS         = "init_ns"
+OPT_INIT_NS_SHORT   = str("-" + OPT_INIT_NS[:1])
+OPT_INIT_NS_EXT     = str("--" + OPT_INIT_NS)
+OPT_INIT_NS_SAME_AS = [
+    OPT_READ,
+    OPT_FILTRATE
+]
+OPT_INIT_NS_SAME_AS_CLI = [
+    OPT_READ_SHORT,
+    OPT_FILTRATE_SHORT
+]
+# predict
+OPT_PREDICT       = "predict"
+OPT_PREDICT_SHORT = str("-" + OPT_PREDICT[:1])
+OPT_PREDICT_EXT   = str("--" + OPT_PREDICT)
+
+# test
+OPT_TEST       = "test"
+OPT_TEST_SHORT = str("-" + OPT_TEST[:1])
+OPT_TEST_EXT   = str("--" + OPT_TEST)
+
+# all operations
+OPS = [
+    OPT_INIT_NS,
+    OPT_READ,
+    OPT_FILTRATE,
+    OPT_PREDICT,
+    OPT_TEST
+]
+
+
+
 # set the CLI parser
 #
 def triplexer_parser():
@@ -58,7 +117,7 @@ def triplexer_parser():
     # system setting arguments end
 
 
-    # option arguments start
+    # operation arguments start
     #
     parser.add_argument_group("operations (require "
         + OPT_NAMESPACE_SHORT + ")")
@@ -100,7 +159,7 @@ def triplexer_parser():
         default=argparse.SUPPRESS,
         help="test stability of predicted triplexes")
     #
-    # option arguments end
+    # operation arguments end
 
 
     # namespace arguments start
@@ -121,3 +180,28 @@ def triplexer_parser():
 
     return parser
 
+
+
+# print the supported namespaces
+#
+def get_supported_namespaces():
+    """
+    Prints the supported namespaces.
+    """
+
+    line_divider = "+-------+----------------------------------+\n"
+
+    supported_namespaces = line_divider
+
+    supported_namespaces += str("|  {:<1}\t| {:<32} |\n".format(
+        "NS", "database:version:organism:genome"))
+
+    supported_namespaces += line_divider
+
+    for k, v in NAMESPACES.items():
+        label, num = v
+        supported_namespaces += "|  {:<1}\t| {:<32} |\n".format(k, v[STRING])
+
+    supported_namespaces += line_divider
+
+    return supported_namespaces
